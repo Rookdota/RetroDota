9ed4c386f7b0cd61dec319b60894f0c8d7b7c8de
Rookdota <pie4all88@gmail.com>
2015-02-15 12:35:25 -0600

    Minor update.

--- game/dota_addons/twenty_seven_spell_invoker_wars/scripts/vscripts/heroes/hero_invoker/quas.lua
+++ game/dota_addons/twenty_seven_spell_invoker_wars/scripts/vscripts/heroes/hero_invoker/quas.lua
@@ -4,6 +4,6 @@
 	Called when Quas is cast.
 ================================================================================================================= ]]
 function invoker_quas_retro_on_spell_start(keys)
-	
+	keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, "modifier_invoker_quas_retro", nil)
 	
 end

53028f0f657e1446aec04b77246c69a61b473d63
Rookdota <pie4all88@gmail.com>
2015-02-15 13:28:44 -0600

    Fixed some bugs and added Wex and Exort.

--- game/dota_addons/twenty_seven_spell_invoker_wars/scripts/vscripts/heroes/invoker.lua
+++ game/dota_addons/twenty_seven_spell_invoker_wars/scripts/vscripts/heroes/invoker.lua
@@ -13,11 +13,11 @@ function invoker_remove_oldest_orb(keys)
 	if keys.caster.invoked_orbs[1] ~= nil then
 		local orb_name = keys.caster.invoked_orbs[1]:GetName()
 		if orb_name == "invoker_quas_retro" then
-			keys.caster:RemoveModifierByName("modifier_invoker_quas_retro")
+			keys.caster:RemoveModifierByName("modifier_invoker_quas_instance_retro")
 		elseif orb_name == "invoker_wex_retro" then
-			keys.caster:RemoveModifierByName("modifier_invoker_wex_retro")
+			keys.caster:RemoveModifierByName("modifier_invoker_wex_instance_retro")
 		elseif orb_name == "invoker_exort_retro" then
-			keys.caster:RemoveModifierByName("modifier_invoker_exort_retro")
+			keys.caster:RemoveModifierByName("modifier_invoker_exort_instance_retro")
 		end
 	end
 end
@@ -29,13 +29,41 @@ end
 	Called when Quas is cast.
 ================================================================================================================= ]]
 function invoker_quas_retro_on_spell_start(keys)
-	if keys.caster.invoked_orbs == nil then
-		keys.caster.invoked_orbs = {}
-	end
-	
 	invoker_remove_oldest_orb(keys)
 	
-	keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_invoker_quas_retro", nil)
+	keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_invoker_quas_instance_retro", nil)
+	
+	keys.caster.invoked_orbs[1] = keys.caster.invoked_orbs[2]
+	keys.caster.invoked_orbs[2] = keys.caster.invoked_orbs[3]
+	keys.caster.invoked_orbs[3] = keys.ability
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: February 15, 2015
+	Called when Wex is cast.
+================================================================================================================= ]]
+function invoker_wex_retro_on_spell_start(keys)
+	invoker_remove_oldest_orb(keys)
+	
+	keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_invoker_wex_instance_retro", nil)
+	
+	keys.caster.invoked_orbs[1] = keys.caster.invoked_orbs[2]
+	keys.caster.invoked_orbs[2] = keys.caster.invoked_orbs[3]
+	keys.caster.invoked_orbs[3] = keys.ability
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: February 15, 2015
+	Called when Exort is cast.
+================================================================================================================= ]]
+function invoker_exort_retro_on_spell_start(keys)
+	invoker_remove_oldest_orb(keys)
+	
+	keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_invoker_exort_instance_retro", nil)
 	
 	keys.caster.invoked_orbs[1] = keys.caster.invoked_orbs[2]
 	keys.caster.invoked_orbs[2] = keys.caster.invoked_orbs[3]

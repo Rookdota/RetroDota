011ff676acff0c5814c68aa71d4cb033f4a12401
Rookdota <pie4all88@gmail.com>
2015-02-20 16:26:14 -0600

    Refactored Portal so it uses a modifier that can be dispelled.

--- game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_portal.lua
+++ game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_portal.lua
@@ -6,30 +6,37 @@
 ================================================================================================================= ]]
 function invoker_retro_portal_on_spell_start(keys)
 	local portal_particle_effect = ParticleManager:CreateParticle("particles/heroes/hero_invoker/invoker_retro_portal.vpcf", PATTACH_ABSORIGIN_FOLLOW, keys.target)
-	keys.target:EmitSound("Hero_Meepo.Poof.Channel")
-
+	
+	--Remove the Portal particle after the duration is supposed to end.
 	Timers:CreateTimer({
-		endTime = keys.Delay,
+		endTime = keys.Delay + .2,
 		callback = function()
-			--Portal's damage is dependent on the level of Wex.
-			local wex_ability = keys.caster:FindAbilityByName("invoker_retro_wex")
-			local portal_damage = 0
-			if wex_ability ~= nil then
-				portal_damage = keys.ability:GetLevelSpecialValueFor("damage", wex_ability:GetLevel() - 1)
-			end
-			
-			FindClearSpaceForUnit(keys.target, keys.caster:GetAbsOrigin(), false)  --Move the target to Invoker's position.
-			keys.target:EmitSound("Hero_Meepo.Poof.End")
-			
-			ApplyDamage({victim = keys.target, attacker = keys.caster, damage = portal_damage, damage_type = DAMAGE_TYPE_MAGICAL,})
-			
-			--Remove the Portal particle after the duration ends.
-			Timers:CreateTimer({
-				endTime = .2,
-				callback = function()
-					ParticleManager:DestroyParticle(portal_particle_effect, false)
-				end
-			})
+			ParticleManager:DestroyParticle(portal_particle_effect, false)
 		end
 	})
+	
+	keys.target:EmitSound("Hero_Meepo.Poof.Channel")
+	keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, "modifier_invoker_retro_portal", nil)
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: February 20, 2015
+	Called a short while after Portal is cast on a unit.  Damages the target and moves them to Invoker.
+================================================================================================================= ]]
+function modifier_invoker_retro_portal_on_interval_think(keys)
+	--Portal's damage is dependent on the level of Wex.
+	local wex_ability = keys.caster:FindAbilityByName("invoker_retro_wex")
+	local portal_damage = 0
+	if wex_ability ~= nil then
+		portal_damage = keys.ability:GetLevelSpecialValueFor("damage", wex_ability:GetLevel() - 1)
+	end
+	
+	FindClearSpaceForUnit(keys.target, keys.caster:GetAbsOrigin(), false)  --Move the target to Invoker's position.
+	keys.target:EmitSound("Hero_Meepo.Poof.End")
+	
+	ApplyDamage({victim = keys.target, attacker = keys.caster, damage = portal_damage, damage_type = DAMAGE_TYPE_MAGICAL,})
+	
+	keys.target:RemoveModifierByNameAndCaster("modifier_invoker_retro_portal", keys.caster)
 end

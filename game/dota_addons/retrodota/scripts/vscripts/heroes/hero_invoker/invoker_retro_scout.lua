e85218f3ceda5d468bbea81a813e4e55d0d0ce44
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-02-22 03:28:57 -0300

    Added scaling ms in Scout/normalized utf8 new d2mk

--- game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_scout.lua
+++ game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_scout.lua
@@ -1,9 +1,9 @@
 --[[
-	Author: Noya
+	Author: Noya and wFX with help of Rook
 	Date: 18.01.2015.
 	Gets the summoning location for the new unit
 ]]
-function SummonLocation( event )
+function SummonLocation(event)
     local caster = event.caster
     local fv = caster:GetForwardVector()
     local origin = caster:GetAbsOrigin()
@@ -18,12 +18,38 @@ function SummonLocation( event )
 end
 
 -- Set the units looking at the same point of the caster
-function SetUnitsMoveForward( event )
+function SetUnitsMoveForward(event)
 	local caster = event.caster
 	local target = event.target
     local fv = caster:GetForwardVector()
     local origin = caster:GetAbsOrigin()
-	
+
 	target:SetForwardVector(fv)
 
+end
+
+function invoker_retro_scout_on_spell_start(event)
+    local caster = event.caster
+    local ability = event.ability
+    local wex_ability = caster:FindAbilityByName("invoker_retro_wex")
+    if wex_ability ~= nil then
+
+        local owl = CreateUnitByName("npc_dota_invoker_retro_scout_unit", caster:GetAbsOrigin(), true, nil, nil, caster:GetTeam())
+        local wex_level = wex_ability:GetLevel()
+
+        for i = 0, wex_level, 1 do
+            ability:ApplyDataDrivenModifier(caster, owl, "modifier_invoker_retro_scout_movespeed_per_wex", {duration = -1})
+        end
+
+--      Set the dymanic vision
+--      local owl_vision = 300 + (wex_level * 100)
+--      TODO: Make the pure scale and check why the other 2 functions are popping errors
+        owl.vOwner = caster:GetOwner()
+        owl:SetControllableByPlayer(caster:GetOwner():GetPlayerID(), true)
+        owl:AddNewModifier(owl, nil, "modifier_kill", {duration = wex_level * 10})
+
+        SummonLocation(event)
+        SetUnitsMoveForward(event)
+
+    end
 end

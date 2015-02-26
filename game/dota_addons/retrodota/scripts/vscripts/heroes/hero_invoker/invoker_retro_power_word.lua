9094cc7ece3a60e91d9cb8fd3761af605a198a11
Rookdota <pie4all88@gmail.com>
2015-02-26 01:23:15 -0600

    Tweaked particle effects for Invis Aura, Portal, and Power Word.
    Also gave some targeted abilities cast ranges that were in need of them.

--- game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_power_word.lua
+++ game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_power_word.lua
@@ -8,10 +8,20 @@ function invoker_retro_power_word_on_spell_start(keys)
 		if keys.caster ~= keys.target then  --If Power Word wasn't self-casted.
 			keys.target:EmitSound("DOTA_Item.MedallionOfCourage.Activate")
 			
+			local power_word_ally_particle_effect = ParticleManager:CreateParticle("particles/units/heroes/hero_invoker/invoker_retro_power_word_ally.vpcf", PATTACH_ABSORIGIN_FOLLOW, keys.target)
+			ParticleManager:SetParticleControlEnt(power_word_ally_particle_effect, 1, keys.target, PATTACH_ABSORIGIN_FOLLOW, "follow_origin", keys.target:GetAbsOrigin(), false)
+			--Remove the particle effect after a short duration.
+			Timers:CreateTimer({
+				endTime = 2,
+				callback = function()
+					ParticleManager:DestroyParticle(power_word_ally_particle_effect, false)
+				end
+			})
+			
 			local quas_ability = keys.caster:FindAbilityByName("invoker_retro_quas")
 			if quas_ability ~= nil then
 				local armor_buff_value = math.abs(keys.ability:GetLevelSpecialValueFor("armor_bonus_ally", quas_ability:GetLevel() - 1))
-				for i=0, armor_buff_value, 1 do
+				for i=1, armor_buff_value, 1 do
 					keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, "modifier_invoker_retro_power_word_armor_buff", nil)
 				end
 				keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, "modifier_invoker_retro_power_word_armor_buff_icon", nil)
@@ -27,10 +37,20 @@ function invoker_retro_power_word_on_spell_start(keys)
 	else  --If Power Word was cast on an enemy.
 		keys.target:EmitSound("DOTA_Item.MedallionOfCourage.Activate")
 		
+		local power_word_enemy_particle_effect = ParticleManager:CreateParticle("particles/units/heroes/hero_invoker/invoker_retro_power_word_enemy.vpcf", PATTACH_ABSORIGIN_FOLLOW, keys.target)
+		ParticleManager:SetParticleControlEnt(power_word_enemy_particle_effect, 1, keys.target, PATTACH_ABSORIGIN_FOLLOW, "follow_origin", keys.target:GetAbsOrigin(), false)
+		--Remove the particle effect after a short duration.
+		Timers:CreateTimer({
+			endTime = 2,
+			callback = function()
+				ParticleManager:DestroyParticle(power_word_enemy_particle_effect, false)
+			end
+		})
+		
 		local quas_ability = keys.caster:FindAbilityByName("invoker_retro_quas")
 		if quas_ability ~= nil then
 			local armor_debuff_value = math.abs(keys.ability:GetLevelSpecialValueFor("armor_reduction_enemy", quas_ability:GetLevel() - 1))
-			for i=0, armor_debuff_value, 1 do
+			for i=1, armor_debuff_value, 1 do
 				keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, "modifier_invoker_retro_power_word_armor_debuff", nil)
 			end
 			keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, "modifier_invoker_retro_power_word_armor_debuff_icon", nil)

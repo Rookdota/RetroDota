c41f04fa787549fd80c2c2db44ef472bbe81369a
Rookdota <pie4all88@gmail.com>
2015-02-27 01:12:44 -0600

    Started Chaos Meteor but need clarification on how its DoT works.

---
+++ game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_chaos_meteor.lua
@@ -0,0 +1,9 @@
+--[[ ============================================================================================================
+	Author: Rook
+	Date: February 26, 2015
+	Called when Chaos Meteor is cast.
+	Additional parameters: 
+================================================================================================================= ]]
+function invoker_retro_chaos_meteor_on_spell_start(keys)
+	
+end

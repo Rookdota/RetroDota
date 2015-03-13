a0f2486202f775d255f01129b6f191fd9689fcde
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-03-13 02:23:46 -0300

    Code for custom particles

--- game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_soul_reaver.lua
+++ game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_soul_reaver.lua
@@ -4,12 +4,14 @@
 	Called when Soul Reaver is cast.
 ================================================================================================================= ]]
 function invoker_retro_soul_reaver_on_spell_start(event)
+
 	local damageTable = {
 		victim = event.target,
 		attacker = event.caster,
 		damage = event.ability:GetAbilityDamage(),
 		damage_type = event.ability:GetAbilityDamageType()
 	}
+
 	ApplyDamage(damageTable)
 	event.ability:ApplyDataDrivenModifier(event.caster, event.caster, "modifier_invoker_retro_soul_reaver", {duration = 8})	
 	event.caster:SetModifierStackCount("modifier_invoker_retro_soul_reaver", event.ability, event.caster:FindAbilityByName("invoker_retro_wex"):GetLevel())
@@ -25,8 +27,11 @@ function invoker_retro_soul_reaver_on_spell_start(event)
 				damage_type = event.ability:GetAbilityDamageType()
 			}
 			ApplyDamage(damageTable)
+
+		local soul_reaver_number = ParticleManager:CreateParticle("particles/units/heroes/hero_invoker/invoker_retro_soul_reaver_msg.vpcf", PATTACH_OVERHEAD_FOLLOW, event.target)
+		ParticleManager:SetParticleControl(soul_reaver_number, 1, Vector(1, damageTable.damage, 0))
+		ParticleManager:SetParticleControl(soul_reaver_number, 2, Vector(2, string.len(math.floor(damageTable.damage)) + 1, 0))
 		end
 	})
-	
-	--keys.target:EmitSound("")
+
 end

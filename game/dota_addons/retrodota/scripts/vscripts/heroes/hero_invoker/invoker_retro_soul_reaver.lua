d09070b37e9d50a5e1aef8f1986369cba2e35916
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-03-13 00:00:34 -0300

    Finished Soul Reaver

--- game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_soul_reaver.lua
+++ game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_soul_reaver.lua
@@ -13,4 +13,20 @@ function invoker_retro_soul_reaver_on_spell_start(event)
 	ApplyDamage(damageTable)
 	event.ability:ApplyDataDrivenModifier(event.caster, event.caster, "modifier_invoker_retro_soul_reaver", {duration = 8})	
 	event.caster:SetModifierStackCount("modifier_invoker_retro_soul_reaver", event.ability, event.caster:FindAbilityByName("invoker_retro_wex"):GetLevel())
+	local particle = ParticleManager:CreateParticle(event.effect_name, PATTACH_ABSORIGIN_FOLLOW, event.target)
+	Timers:CreateTimer({
+		endTime = 8,
+		callback = function()
+			ParticleManager:DestroyParticle(particle, false)
+			local damageTable = {
+				victim = event.target,
+				attacker = event.caster,
+				damage = event.ability:GetLevelSpecialValueFor("after_damage", event.caster:FindAbilityByName("invoker_retro_exort"):GetLevel()),
+				damage_type = event.ability:GetAbilityDamageType()
+			}
+			ApplyDamage(damageTable)
+		end
+	})
+	
+	--keys.target:EmitSound("")
 end

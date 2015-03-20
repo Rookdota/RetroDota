2ed49e5dfe5610d9cda054dd579ac6f5fee646c2
MNoya <martinnoya@gmail.com>
2015-03-20 11:57:52 -0300

    Fixed invulnerability script (I think)

--- game/dota_addons/retrodota/scripts/vscripts/buildings/tower.lua
+++ game/dota_addons/retrodota/scripts/vscripts/buildings/tower.lua
@@ -8,20 +8,45 @@ function ApplyInvulnerability( event )
 	local caster = event.caster
 	caster:AddNewModifier(caster, nil, "modifier_invulnerable", {})
 
-	-- Add 1 to the Ancient Invuln Count
-	if GameRules.AncientInvulnerability then
-		GameRules.AncientInvulnerability = GameRules.AncientInvulnerability + 1
-	else
-		GameRules.AncientInvulnerability = 1
+	-- Add 1 to the Tree Invuln Count
+	if caster:GetTeamNumber() == DOTA_TEAM_GOODGUYS then
+		if GameRules.TreeInvulnerability then
+			GameRules.TreeInvulnerability = GameRules.TreeInvulnerability + 1
+		else
+			GameRules.TreeInvulnerability = 1
+		end
+	elseif caster:GetTeamNumber() == DOTA_TEAM_BADGUYS then
+		if GameRules.ThroneInvulnerability then
+			GameRules.ThroneInvulnerability = GameRules.ThroneInvulnerability + 1
+		else
+			GameRules.ThroneInvulnerability = 1
+		end
+		
 	end
 end
 
 function ReduceInvulnerabilityCount( event )
-	GameRules.AncientInvulnerability = GameRules.AncientInvulnerability - 1
+	local caster = event.caster
+	local ancients = Entities:FindAllByClassname('npc_dota_fort')
 
-	if GameRules.AncientInvulnerability <= 0 then
-		local caster = event.caster
-		local ancients = Entities:FindAllByClassname('npc_dota_fort')
+	if caster:GetTeamNumber() == DOTA_TEAM_GOODGUYS then
+		GameRules.TreeInvulnerability = GameRules.TreeInvulnerability - 1
+		print("Reduced Tree Invulnerability Count to "..GameRules.TreeInvulnerability)
+
+	elseif caster:GetTeamNumber() == DOTA_TEAM_BADGUYS then
+		GameRules.ThroneInvulnerability = GameRules.ThroneInvulnerability - 1
+		print("Reduced Throne Invulnerability Count to "..GameRules.ThroneInvulnerability)
+	end
+
+	
+	if GameRules.ThroneInvulnerability <= 0 then		
+		for k,v in pairs(ancients) do
+			print(k,v,v:GetTeam(),caster:GetTeam())
+	        if v:GetTeam() == caster:GetTeam()then
+	            v:RemoveModifierByName("modifier_invulnerable")
+	        end
+	    end
+	elseif GameRules.TreeInvulnerability <= 0 then
 		for k,v in pairs(ancients) do
 			print(k,v,v:GetTeam(),caster:GetTeam())
 	        if v:GetTeam() == caster:GetTeam()then

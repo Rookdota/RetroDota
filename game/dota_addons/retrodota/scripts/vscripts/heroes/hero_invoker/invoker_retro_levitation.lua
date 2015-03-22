d980675497eb1fea2de2a8192f92298db27e57a0
Rookdota <pie4all88@gmail.com>
2015-03-21 21:14:42 -0500

    Fixed the Levitation visual bug.

--- game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_levitation.lua
+++ game/dota_addons/retrodota/scripts/vscripts/heroes/hero_invoker/invoker_retro_levitation.lua
@@ -47,47 +47,59 @@ function TornadoHeight( keys )
 	local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
-	
-	local cyclone_height = ability:GetLevelSpecialValueFor( "cyclone_height" , ability:GetLevel() - 1 )
-	local cyclone_min_height = ability:GetLevelSpecialValueFor( "cyclone_min_height" , ability:GetLevel() - 1 )
-	local cyclone_max_height = ability:GetLevelSpecialValueFor( "cyclone_max_height" , ability:GetLevel() - 1 )
-	local tornado_start = GameRules:GetGameTime()
 
 	-- Position variables
-	local target_initial_x = target:GetAbsOrigin().x
-	local target_initial_y = target:GetAbsOrigin().y
-	local target_initial_z = target:GetAbsOrigin().z
-	local position = Vector(target_initial_x, target_initial_y, target_initial_z)
-
+	local target_origin = target:GetAbsOrigin()
+	local target_initial_x = target_origin.x
+	local target_initial_y = target_origin.y
+	local target_initial_z = target_origin.z
+	local position = Vector(target_initial_x, target_initial_y, target_initial_z)  --This is updated whenever the target has their position changed.
+	
 	local duration = 0
 	
-	--Levitate's duration is dependent on the levels of Quas, Wex, and Exort.
+	--Levitation's duration is dependent on the levels of Quas, Wex, and Exort.
 	local quas_ability = keys.caster:FindAbilityByName("invoker_retro_quas")
 	local wex_ability = keys.caster:FindAbilityByName("invoker_retro_wex")
 	local exort_ability = keys.caster:FindAbilityByName("invoker_retro_exort")
 	
+	local average_level = 0
 	if quas_ability ~= nil and wex_ability ~= nil and exort_ability ~= nil then
-		local average_level = (quas_ability:GetLevel() + wex_ability:GetLevel() + exort_ability:GetLevel()) / 3
+		average_level = (quas_ability:GetLevel() + wex_ability:GetLevel() + exort_ability:GetLevel()) / 3
 		average_level = math.floor(average_level + .5)  --Round to the nearest integer.
 		
 		--Ensure that the average level is in-bounds, just in case.
 		if average_level < 1 then
 			average_level = 1
 		end
-		if average_level > 7 then
-			average_level = 7
+		if average_level > 8 then
+			average_level = 8
 		end
 		
 		duration = keys.ability:GetLevelSpecialValueFor("duration", average_level - 1)
 	end
+	
+	local ground_position = GetGroundPosition(position, target)
+	local cyclone_initial_height = ability:GetLevelSpecialValueFor("cyclone_initial_height" , average_level - 1) + ground_position.z
+	local cyclone_min_height = ability:GetLevelSpecialValueFor("cyclone_min_height" , average_level - 1) + ground_position.z
+	local cyclone_max_height = ability:GetLevelSpecialValueFor("cyclone_max_height" , average_level - 1) + ground_position.z
+	local tornado_start = GameRules:GetGameTime()
 
 	-- Height per time calculation
-	local time_to_reach_max_height = duration / 10
-	local height_per_frame = cyclone_height * 0.03
-	--print(height_per_frame)
+	local time_to_reach_initial_height = duration / 10  --1/10th of the total Levitation duration will be spent ascending and descending to and from the initial height.
+	local initial_ascent_height_per_frame = ((cyclone_initial_height - position.z) / time_to_reach_initial_height) * .03  --This is the height to add every frame when Levitation is first cast, and applies until the caster reaches their max height.
+	
+	local up_down_cycle_height_per_frame = initial_ascent_height_per_frame / 3  --This is the height to add or remove every frame while the caster is in up/down cycle mode.
+	print("up/down height per frame: " .. up_down_cycle_height_per_frame)
+	if up_down_cycle_height_per_frame > 7.5 then  --Cap this value so the unit doesn't jerk up and down for short-duration levitations.
+		up_down_cycle_height_per_frame = 7.5
+	end
+	
+	local final_descent_height_per_frame = nil  --This is calculated when the unit begins descending.
+	--print(initial_ascent_height_per_frame)
+	--print(up_down_cycle_height_per_frame)
 
 	-- Time to go down
-	local time_to_stop_fly = duration - time_to_reach_max_height
+	local time_to_stop_fly = duration - time_to_reach_initial_height
 	--print(time_to_stop_fly)
 
 	-- Loop up and down
@@ -97,19 +109,28 @@ function TornadoHeight( keys )
 	Timers:CreateTimer(function()
 		local time_in_air = GameRules:GetGameTime() - tornado_start
 		
-		-- First send the target at max height very fast
-		if position.z < cyclone_height and time_in_air <= time_to_reach_max_height then
-			--print("+",height_per_frame,position.z)
+		-- First send the target to the cyclone's initial height.
+		if position.z < cyclone_initial_height and time_in_air <= time_to_reach_initial_height then
+			print("+",initial_ascent_height_per_frame,position.z)
 			
-			position.z = position.z + height_per_frame
+			position.z = position.z + initial_ascent_height_per_frame
 			target:SetAbsOrigin(position)
 			return 0.03
 
-		-- Go down until the target reaches the initial z
+		-- Go down until the target reaches the ground.
 		elseif time_in_air > time_to_stop_fly and time_in_air <= duration then
-			--print("-",height_per_frame)
-
-			position.z = position.z - height_per_frame
+			--Since the unit may be anywhere between the cyclone's min and max height values when they start descending to the ground,
+			--the descending height per frame must be calculated when that begins, so the unit will end up right on the ground when the duration is supposed to end.
+			if final_descent_height_per_frame == nil then
+				local descent_initial_height_above_ground = position.z - ground_position.z
+				--print("ground position: " .. GetGroundPosition(position, target).z)
+				--print("position.z : " .. position.z)
+				final_descent_height_per_frame = (descent_initial_height_above_ground / time_to_reach_initial_height) * .03
+			end
+			
+			print("-",final_descent_height_per_frame,position.z)
+			
+			position.z = position.z - final_descent_height_per_frame
 			target:SetAbsOrigin(position)
 			return 0.03
 
@@ -118,7 +139,7 @@ function TornadoHeight( keys )
 			-- Up
 			if position.z < cyclone_max_height and going_up then 
 				--print("going up")
-				position.z = position.z + height_per_frame/3
+				position.z = position.z + up_down_cycle_height_per_frame
 				target:SetAbsOrigin(position)
 				return 0.03
 
@@ -126,7 +147,7 @@ function TornadoHeight( keys )
 			elseif position.z >= cyclone_min_height then
 				going_up = false
 				--print("going down")
-				position.z = position.z - height_per_frame/3
+				position.z = position.z - up_down_cycle_height_per_frame
 				target:SetAbsOrigin(position)
 				return 0.03
 
@@ -139,6 +160,7 @@ function TornadoHeight( keys )
 
 		-- End
 		else
+			print(GetGroundPosition(target:GetAbsOrigin(), target))
 			--print("End TornadoHeight")
 		end
 	end)

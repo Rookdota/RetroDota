2fb81381984ed3a7da38b20e964640382890b6ab
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-03-25 19:02:09 -0300

    Finished Ante up and Lucky Stars
    Missing particles and sounds
    All in chance values fixed
    Ante up buffs fixed
    Lucky stars code done
    TODO: lookout for ally trigger of lucky stars, and possible bug of
    techies suicide counting 2 times in ante up.

--- game/dota_addons/retrodota/scripts/vscripts/heroes/hero_gambler/gambler_retro_ante_up.lua
+++ game/dota_addons/retrodota/scripts/vscripts/heroes/hero_gambler/gambler_retro_ante_up.lua
@@ -7,10 +7,10 @@ function gambler_retro_ante_up_on_spell_start(event)
 	local caster = event.caster
 	local target = event.target
 	local ability  = event.ability
-	local args
+	local arg
 	if caster:GetTeamNumber() == target:GetTeamNumber() then
 		target:Heal(ability:GetAbilityDamage(), caster)
-		args = {IsBuff = 1, IsDebuff = 0}
+		arg = "modifier_gambler_retro_ante_up_buff"
 	else
 		ApplyDamage({
 			victim = target,
@@ -18,9 +18,9 @@ function gambler_retro_ante_up_on_spell_start(event)
 			damage = ability:GetAbilityDamage(),
 			damage_type = ability:GetAbilityDamageType()
 		})
-		args = {IsBuff = 0, IsDebuff = 1}
+		arg = "modifier_gambler_retro_ante_up_debuff"
 	end
-	ability:ApplyDataDrivenModifier(caster, target,"modifier_gambler_retro_ante_up", args)
+	ability:ApplyDataDrivenModifier(caster, target, arg, {})
 end
 
 
@@ -31,7 +31,7 @@ end
 	Called when the owner of ante up kill another hero.
 ================================================================================================================= ]]
  function gambler_retro_ante_up_on_owner_hero_kill(event)
-	event.caster:SetGold(event.ability:GetSpecialValueFor("cash_in"), false)
+	event.caster:ModifyGold(event.ability:GetSpecialValueFor("cash_in"), false, 0)
 	event.unit:RemoveModifierByNameAndCaster("modifier_gambler_retro_ante_up", event.caster)
 end
 
@@ -41,6 +41,6 @@ end
 	Called when the owner of ante up die.
 ================================================================================================================= ]]
 function gambler_retro_ante_up_on_owner_death(event)
-	event.caster:SetGold(event.ability:GetSpecialValueFor("cash_in")/2, false)
+	event.caster:ModifyGold((event.ability:GetSpecialValueFor("cash_in")/2), false, 0)
 	event.unit:RemoveModifierByNameAndCaster("modifier_gambler_retro_ante_up", event.caster)
 end
--- game/dota_addons/retrodota/scripts/vscripts/heroes/hero_gambler/gambler_retro_lucky_stars.lua
+++ game/dota_addons/retrodota/scripts/vscripts/heroes/hero_gambler/gambler_retro_lucky_stars.lua
@@ -1,2 +1,42 @@
+function gambler_retro_lucky_stars_on_success(event)
+	-- TODO: Confirm if it's ok to trigger on allies
+	if event.target:IsCreature() then
+		event.target:Kill(event.ability, event.caster)
+	elseif event.target:IsHero() or event.target:IsTower() or event.target.GetInvulnCount ~= nil then
+		ApplyDamage({
+			victim = event.target,
+			attacker = event.caster,
+			damage = event.ability:GetSpecialValueFor("bonus_damage"),
+			damage_type = event.ability:GetAbilityDamageType()
+		})
+	end
+end
+
 function gambler_retro_lucky_stars_pillage(event)
+	-- TODO: Confirm if it's ok to get gold by denying/hitting allied units
+	if event.caster.pillaged_gold == nil then
+		event.caster.pillaged_gold = 0
+	end
+
+	-- print("-------------------------------")
+	-- print("Before Pillaged GOLD:" .. event.caster.pillaged_gold)
+	local pillage = (event.attack_damage/event.target:GetMaxHealth()) * event.pillage_ratio * event.target:GetGoldBounty()/2
+	event.caster.pillaged_gold = event.caster.pillaged_gold + pillage
+	local effective_gold = math.floor(event.caster.pillaged_gold)
+
+	-- print("AD: " .. event.attack_damage)
+	-- print("MHP: " .. event.target:GetMaxHealth())
+	-- print("PR: " .. event.pillage_ratio)
+	-- print("BH: " .. event.target:GetGoldBounty())
+	-- print("PILLAGE: " .. pillage)
+	-- print("Before GOLD:" .. event.caster:GetGold())	
+	-- print("Effective: " .. effective_gold)
+
+	if effective_gold > 0 then
+		-- print("GREATER THAN 0 MAKING STUFF")
+		event.caster:ModifyGold(effective_gold, false, 0)
+		event.caster.pillaged_gold = event.caster.pillaged_gold - effective_gold
+	end
+	-- print("After GOLD:" .. event.caster:GetGold())
+	-- print("After Pillaged GOLD:" .. event.caster.pillaged_gold)
 end
